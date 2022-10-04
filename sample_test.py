8d1198f5edce7bc615f0cccb9390726b5e28a83e
Varvara Dvortsova <dvortsova.varvara@yandex.ru>
2022-10-04 10:39:27 +0300

    Add fail tests

--- sample_test.py
+++ sample_test.py
@@ -15,8 +15,8 @@ class TestMyMath(unittest.TestCase):
   def test_multiplication(self):
     self.assertEqual(MyMath.multiply(3, 4), 12)
 
-  # def test_failure(self):
-  #   self.assertEqual(2 + 2, 3)
+  def test_failure(self):
+    self.assertEqual(2 + 2, 3)
 
   def test_fibonacci_slow(self):
     MyMath.fibonacci(30)
